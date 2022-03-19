d26e59297640d16c71db2de1b184f08ca304b641
Jay <2659543816@qq.com>
2022-03-19 15:11:03 +0800

    second commit

--- src/main/java/com/xibeiminmzu/git/GitText.java
+++ src/main/java/com/xibeiminmzu/git/GitText.java
@@ -11,5 +11,6 @@ package com.xibeiminmzu.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git 2");
     }
 }
