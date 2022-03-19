861e81e995e14bb23d30a98bd6d9d6b5e20ea171
Jay <2659543816@qq.com>
2022-03-19 15:17:58 +0800

    hot-fix first commit

--- src/main/java/com/xibeiminmzu/git/GitText.java
+++ src/main/java/com/xibeiminmzu/git/GitText.java
@@ -13,5 +13,6 @@ public class GitText {
         System.out.println("hello Git!");
         System.out.println("hello Git 2");
         System.out.println("hello Git3");
+        System.out.println("Hello Git4");
     }
 }
