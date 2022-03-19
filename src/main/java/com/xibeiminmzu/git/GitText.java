9507e593e809f10442f66ad9e07c01958c7b9d30
Jay <2659543816@qq.com>
2022-03-19 15:11:41 +0800

    third commit

--- src/main/java/com/xibeiminmzu/git/GitText.java
+++ src/main/java/com/xibeiminmzu/git/GitText.java
@@ -12,5 +12,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello Git 2");
+        System.out.println("hello Git3");
     }
 }
