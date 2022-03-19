6ba5d543b339e934cc7789d03f746c12dcce15de
Jay <2659543816@qq.com>
2022-03-19 15:09:18 +0800

    first commit

---
+++ src/main/java/com/xibeiminmzu/git/GitText.java
@@ -0,0 +1,15 @@
+package com.xibeiminmzu.git;
+
+/**
+ * @PackageName:com.xibeiminmzu.git
+ * @ClassName:GitText
+ * @Auth Jay
+ * @Data 2022/3/19 15:05
+ * @Function //
+ * @Version 1.0
+ */
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
